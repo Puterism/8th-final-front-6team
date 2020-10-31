46054d8f2b5cf9f3c62bd9ac706fde2556236c6f
simsimjae <simsimjae@naver.com>
2020-10-31 15:21:09 +0900

    useReducer삭제

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,10 @@
-import React, { useReducer, useCallback } from 'react';
+import React from 'react';
 import TestComponent from '../components/TestComponent';
-import { testInitialState, testReducer } from '../reducers/testReducer';
 
-const MainPage = () => {
-  const [testState, testDispatch] = useReducer(testReducer, testInitialState);
-
-  const handleTestButtonClick = useCallback(() => {
-    testDispatch({ type: 'CLICK_BUTTON' });
-  }, [testDispatch]);
-
-  return (
-    <div>
-      <TestComponent number={testState.number} isEnd={testState.isEnd} onClickButton={handleTestButtonClick} />
-    </div>
-  );
-};
+const MainPage = () => (
+  <div>
+    <TestComponent />
+  </div>
+);
 
 export default MainPage;
