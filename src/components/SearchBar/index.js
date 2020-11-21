3e209824fb4b2cfd2f9969b992e43de2c5f5bdc1
lee jeongmin <danmin20@khu.ac.kr>
2020-11-21 19:46:23 +0900

    💄SearchBar UI 수정

--- src/components/SearchBar/index.js
+++ src/components/SearchBar/index.js
@@ -1,21 +1,36 @@
-import { Flex, Input, IconButton } from '@chakra-ui/core';
+import { Flex, Input } from '@chakra-ui/core';
 import React, { useState, useCallback } from 'react';
-import { FiSearch } from 'react-icons/fi';
+// import { FiSearch } from 'react-icons/fi';
+import theme from '../../themes';
 
 const inputStyle = { boxShadow: '0 0 1.5px 0 #9ad2ff', color: '#2699fb' };
 
-const SearchBar = () => {
+const SearchBar = ({ placeholder, width }) => {
   const [keyword, setKeyword] = useState('');
-  const isActive = keyword.length > 0;
+  // const isActive = keyword.length > 0;
 
   const handleChange = useCallback((e) => {
     setKeyword(e.target.value);
   }, []);
 
   return (
-    <Flex w="full">
-      <Input bg="white" onChange={handleChange} value={keyword} placeholder="구입할 채소를 검색해주세요. 최저가 조합의 커머스를 알려드릴게요. ex) 양파 + 감자" borderColor="sub" color="main" boxShadow={inputStyle.boxShadow} fontSize="15px" _focus={inputStyle} _hover={inputStyle} _placeholder={{ color: '#2699fb' }} />
-      <IconButton icon={<FiSearch color="white" />} background="main" ml="11px" disabled={!isActive} />
+    <Flex w={width}>
+      <Input
+        bg="white"
+        onChange={handleChange}
+        value={keyword}
+        placeholder={placeholder}
+        borderColor={theme.colors.green}
+        border="solid 2px"
+        borderRadius="30px"
+        color={theme.colors.mediumGray}
+        padding="25px 35px"
+        boxShadow={inputStyle.boxShadow}
+        fontSize="18px"
+        _focus={inputStyle}
+        _hover={inputStyle}
+        _placeholder={{ color: theme.colors.mediumGray }}
+      />
     </Flex>
   );
 };
--- src/pages/MainPage/index.js
+++ src/pages/MainPage/index.js
@@ -7,7 +7,7 @@ const MainPage = () => (
   <Layout>
     <Heading color="main.500">Avocado</Heading>
     <Box mt="76px" w="full">
-      <SearchBar mt="76px" />
+      <SearchBar mt="76px" placeholder="구입할 채소를 검색해주세요. 최저가 조합의 커머스를 알려드릴게요. ex) 양파 + 감자" />
     </Box>
   </Layout>
 );
