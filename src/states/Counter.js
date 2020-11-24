6080b64c9a7750738fbd0a3cdc705084103f9bef
lee jeongmin <danmin20@khu.ac.kr>
2020-11-25 01:25:42 +0900

    ♻️ refactoring

--- src/states/Counter.js
+++ src/states/Counter.js
@@ -1,9 +1,5 @@
 import React from 'react';
 
 export default () => (
-  <div>
-    <p>Counter Component State : 0</p>
-    <button type="button">+</button>
-    <button type="button">-</button>
-  </div>
+  <div />
 );
