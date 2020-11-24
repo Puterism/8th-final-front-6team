7a6ed1540c7e26a89ade16fad591bbd81f89f1a3
lee jeongmin <danmin20@khu.ac.kr>
2020-11-25 01:16:05 +0900

    ➕ recoil 세팅

--- src/index.jsx
+++ src/index.jsx
@@ -1,31 +1,10 @@
-import { ChakraProvider } from '@chakra-ui/core';
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
-import {
-  MainPage, CategoryPage, ResultPage, SelectionPage,
-} from './pages';
-import theme from './themes/index';
-import './index.css';
+import App from './App';
 
 ReactDOM.render(
-  <ChakraProvider theme={theme}>
-    <Router>
-      <Switch>
-        <Route path="/" exact>
-          <MainPage />
-        </Route>
-        <Route path="/category1" exact>
-          <CategoryPage />
-        </Route>
-        <Route path="/result">
-          <ResultPage />
-        </Route>
-        <Route path="/selection" exact>
-          <SelectionPage />
-        </Route>
-      </Switch>
-    </Router>
-  </ChakraProvider>,
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
   document.getElementById('root'),
 );
---
+++ src/states/Counter.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default () => (
+  <div>
+    <p>Counter Component State : 0</p>
+    <button type="button">+</button>
+    <button type="button">-</button>
+  </div>
+);
