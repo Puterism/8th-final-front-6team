ec1754374920707ee6306a03778830b0150c8314
lee jeongmin <danmin20@khu.ac.kr>
2020-11-28 23:58:00 +0900

    🎨 포맷 수정

--- src/components/TotalBox/index.js
+++ src/components/TotalBox/index.js
@@ -3,10 +3,7 @@ import { Flex, Box, Avatar, Text } from '@chakra-ui/core';
 import theme from '../../themes';
 import { SearchIcon } from '../../assets';
 
-const SelectedList = ({
-  name, price, weight, num,
-  isSelected, setSelectedVegi,
-}) => {
+const SelectedList = ({ name, price, weight, num, isSelected, setSelectedVegi }) => {
   const hoverStyle = { bg: theme.colors.subGray };
   return (
     <Box
@@ -29,11 +26,11 @@ const SelectedList = ({
             <Text>{name}</Text>
           </Flex>
           <Flex alignItems="center">
-            <Text fontSize="12px" color="#cccccc">무안 햇양파 국내산 신토...</Text>
+            <Text fontSize="12px" color="#cccccc">
+              무안 햇양파 국내산 신토...
+            </Text>
             <Text ml="auto" textAlign="end" fontSize="15px" color="#aaaaaa">
-              {price}
-              원
-              {' / '}
+              {price}원{' / '}
               {weight}
               {'  '}
               {num}
@@ -45,65 +42,67 @@ const SelectedList = ({
   );
 };
 
-export default ({
-  mallName, totalPrice, list, isSelected,
-  selectedVegi, setSelectedVegi,
-}) => (
+export default ({ mallName, totalPrice, list, isSelected, selectedVegi, setSelectedVegi }) => (
+  <Box
+    border="solid 2px"
+    borderColor={isSelected ? theme.colors.green : theme.colors.subGray}
+    w="387px"
+    h="482px"
+    color={theme.colors.black}
+    display="flex"
+    flexDir="column"
+    alignItems="center"
+    borderRadius="30px"
+    p="16px"
+  >
     <Box
-      border="solid 2px"
-      borderColor={isSelected ? theme.colors.green : theme.colors.subGray}
-      w="387px"
-      h="482px"
-      color={theme.colors.black}
+      w="100%"
+      h="134px"
+      bg={isSelected ? theme.colors.lightGreen : theme.colors.subGray}
       display="flex"
       flexDir="column"
-      alignItems="center"
-      borderRadius="30px"
-      p="16px"
+      borderRadius="19px"
+      p="14px"
     >
-      <Box
-        w="100%"
-        h="134px"
-        bg={isSelected ? theme.colors.lightGreen : theme.colors.subGray}
-        display="flex"
-        flexDir="column"
-        borderRadius="19px"
-        p="14px"
-      >
-        <Flex alignItems="center">
-          <Avatar
-            size="xs"
-            bg="white"
-          />
-          <Text ml="4px" fontSize="16px">{mallName}</Text>
-        </Flex>
-        <Text textAlign="end" mt="8px" fontSize="32px" fontWeight="bold" color={isSelected ? theme.colors.green : '#cccccc'}>
-          {totalPrice}
-          <span style={{ fontSize: '17px', ml: '5px' }}>원</span>
+      <Flex alignItems="center">
+        <Avatar size="xs" bg="white" />
+        <Text ml="4px" fontSize="16px">
+          {mallName}
         </Text>
-
-        <Flex mt="auto" alignItems="center">
-          <Text fontSize="14px" color={isSelected ? theme.colors.green : '#cccccc'}>배송료 2,500원</Text>
-          <Text fontSize="14px" ml="10px" color={isSelected ? theme.colors.green : '#cccccc'}>3만원 이상 무료배송</Text>
-        </Flex>
-      </Box>
-
-      <Box
-        mt="10px"
-        w="full"
-        overflowX="scroll"
+      </Flex>
+      <Text
+        textAlign="end"
+        mt="8px"
+        fontSize="32px"
+        fontWeight="bold"
+        color={isSelected ? theme.colors.green : '#cccccc'}
       >
-        {list.map((item) => (
-          <SelectedList
-            key={item.key}
-            name={item.name}
-            price={item.price}
-            weight={item.weight}
-            num={item.num}
-            isSelected={selectedVegi === item.name}
-            setSelectedVegi={setSelectedVegi}
-          />
-        ))}
-      </Box>
+        {totalPrice}
+        <span style={{ fontSize: '17px', ml: '5px' }}>원</span>
+      </Text>
+
+      <Flex mt="auto" alignItems="center">
+        <Text fontSize="14px" color={isSelected ? theme.colors.green : '#cccccc'}>
+          배송료 2,500원
+        </Text>
+        <Text fontSize="14px" ml="10px" color={isSelected ? theme.colors.green : '#cccccc'}>
+          3만원 이상 무료배송
+        </Text>
+      </Flex>
     </Box>
-  );
+
+    <Box mt="10px" w="full" overflowX="scroll">
+      {list.map(item => (
+        <SelectedList
+          key={item.key}
+          name={item.name}
+          price={item.price}
+          weight={item.weight}
+          num={item.num}
+          isSelected={selectedVegi === item.name}
+          setSelectedVegi={setSelectedVegi}
+        />
+      ))}
+    </Box>
+  </Box>
+);
--- src/pages/MainPage/index.js
+++ src/pages/MainPage/index.js
@@ -6,15 +6,13 @@ import Chip from '../../components/Chip';
 
 const MainPage = () => (
   <Layout>
-    <Flex flexDir="column" mt="180px" w="full" alignItems="center" >
+    <Flex flexDir="column" mt="180px" w="full" alignItems="center">
       <Img src="/images/main.png" w="450px" h="220px" mx="auto" />
       <Heading color="darkGray" fontSize="40px" mt="2">
         for a smarter veggie lifestyle
       </Heading>
       <Box mt="58px" w="full" px="12rem">
-        <SearchBar
-          placeholder="구입할 채소를 검색해주세요. 최저가 조합의 커머스를 알려드릴게요. ex) 양파 + 감자"
-        />
+        <SearchBar placeholder="구입할 채소를 검색해주세요. 최저가 조합의 커머스를 알려드릴게요. ex) 양파 + 감자" />
       </Box>
     </Flex>
   </Layout>
